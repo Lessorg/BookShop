c1ed7367ac162ac506ae64bc3642f021fe6ac485
Nikolai <colya.reznick@gmail.com>
2024-10-26 19:36:50 +0300

    Added fixes

--- src/main/java/test/project/bookshop/BookShopApplication.java
+++ src/main/java/test/project/bookshop/BookShopApplication.java
@@ -3,7 +3,6 @@ package test.project.bookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class BookShopApplication {
 
