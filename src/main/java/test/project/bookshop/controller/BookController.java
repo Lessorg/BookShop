f4914b9fe60ae4c85ecde66443aebf9c26887599
Nikolai <colya.reznick@gmail.com>
2024-10-31 13:31:13 +0200

    Added fixes

--- src/main/java/test/project/bookshop/controller/BookController.java
+++ src/main/java/test/project/bookshop/controller/BookController.java
@@ -2,9 +2,11 @@ package test.project.bookshop.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +27,21 @@ public class BookController {
 
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
-        return bookService.findByid(id);
+        return bookService.findById(id);
     }
 
     @PostMapping
     public BookDto createBook(@RequestBody BookRequestDto requestDto) {
         return bookService.save(requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id, @RequestBody BookRequestDto requestDto) {
+        return bookService.update(id, requestDto);
     }
 }
