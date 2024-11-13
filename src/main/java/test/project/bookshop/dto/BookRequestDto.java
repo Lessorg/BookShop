f67d6ea8ffb0d682d8a7949081e564d2dcab694d
Reznik Nikolai <61866837+Lessorg@users.noreply.github.com>
2024-11-13 17:28:38 +0200

    Initial commit (#6)
    * Initial commit
    
    * Added fixes

--- src/main/java/test/project/bookshop/dto/BookRequestDto.java
+++ src/main/java/test/project/bookshop/dto/BookRequestDto.java
@@ -1,5 +1,7 @@
 package test.project.bookshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +9,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
     private String isbn;
+    @NotBlank
     private String description;
     private String coverImage;
+    @Min(0)
     private BigDecimal price;
 }
