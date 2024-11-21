161e2c4a667f89458cc2e5c0d6cbb0bec0886f39
Nikolai <colya.reznick@gmail.com>
2024-11-21 16:09:56 +0200

    fixed

--- src/main/java/test/project/bookshop/repository/user/UserRepository.java
+++ src/main/java/test/project/bookshop/repository/user/UserRepository.java
@@ -1,11 +1,8 @@
 package test.project.bookshop.repository.user;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import test.project.bookshop.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    boolean existsByEmail(@NotBlank @Email String email);
+    boolean existsByEmail(String email);
 }
